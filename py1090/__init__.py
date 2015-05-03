ab77b843edcc0ad274841d9b7de056165a908b17
Jonas Lieb <lieb.jonas@gmail.com>
2015-05-03 14:36:12 +0200

    py1090: added docstrings, implemented compatibility with RTL1090

--- py1090/__init__.py
+++ py1090/__init__.py
@@ -1,6 +1,6 @@
 """
 This module provides a pythonic way to process ADS-B messages. It can work with any software that provides BaseStation-like output,
-for example `dump1090 <https://github.com/antirez/dump1090>`_.
+for example `dump1090 <https://github.com/MalcolmRobb/dump1090>`_.
 """
 from .connection import Connection
 from .message import Message
--- py1090/collection.py
+++ py1090/collection.py
@@ -1,48 +1,13 @@
 from collections import namedtuple, defaultdict
 from .message import Message
 
-class FlightCollectionEntry:
-	def __init__(self):
-		self.messages = []
-		self.hexident = None
-
-	def append(self, message):
-		if self.hexident is None:
-			self.hexident = message.hexident
-		else:
-			if self.hexident != message.hexident:
-				raise ValueError("Added message of different hexident.")
-
-		self.messages.append(message)
-
-	@property
-	def last_position(self):
-		for message in reversed(self.messages):
-			if message.latitude and message.longitude:
-				return (message.latitude, message.longitude)
-		return None, None
-
-	@property
-	def last_altitude(self):
-		for message in reversed(self.messages):
-			if message.altitude:
-				return message.altitude
-		return None
-
-	@property
-	def path(self):
-		for message in self.messages:
-			lat, lon, alt = message.latitude, message.longitude, message.altitude
-			if lat and lon and alt:
-				yield (lat, lon, alt)
-
-
-	def __iter__(self):
-		return iter(self.messages)
-
-
-
 class FlightCollection:
+	"""A collection of :py:class:`FlightCollectionEntry`'s, stored by hexident.
+
+	It does not provide groundbreaking new features, but it's a helper that stores flights for easy querying.
+	An instance can be queried (``collection['xxxxx']``) and iterated through.
+	"""
+
 	def __init__(self):
 		self._dictionary = defaultdict(FlightCollectionEntry)
 
@@ -59,12 +24,110 @@ class FlightCollection:
 		return iter(self._dictionary.values())
 
 	def add(self, message):
+		"""Adds a message to this collection.
+
+		Args:
+			message (:py:class:`Message`): message to add
+
+		"""
+		if not isinstance(message, Message):
+			message = Message.from_string(message)
 		self._dictionary[message.hexident].append(message)
 
 	def flights(self):
+		"""All stored flights.
+
+		Returns:
+			list: List of :py:class:`py1090.collection.FlightCollectionEntry`
+
+		"""
 		return self._dictionary.values()
 
-	def add_list(self, list):
-		for item in list:
-			message = Message.from_string(item)
+	def add_list(self, iterable):
+		"""Adds multiple messages to the collection.
+
+		Args:
+			iterable (iterable): List, file-like object or :py:class:`Connection` which contains all lines to be added.
+				Calls :py:meth:`Message.from_string` on each item if it is not a :py:class:`Message` already.
+
+		"""
+		for message in iterable:
+			if not isinstance(message, Message):
+				message = Message.from_string(item)
 			self.add(message)
+
+class FlightCollectionEntry:
+	"""Entry of a :py:class:`FlightCollection`. Allows for easy querying flight data, since one single message does not contain all the data
+	information about a flight.
+
+	See Also:
+		:py:class:`Message`
+			For details about the accuracy of the positional information provided by this class.
+
+	"""
+	def __init__(self):
+		self.messages = []
+		self.hexident = None
+
+	def append(self, message):
+		"""Adds a message that should belong to this collection.
+
+		Will raise an exception if the message hexident does not match the one of the flight. Only adds message of type 'MSG'
+		(transmission messages).
+
+		Args:
+			message (Message): the message to append
+
+		"""
+		if self.hexident is None:
+			self.hexident = message.hexident
+		else:
+			if self.hexident != message.hexident:
+				raise ValueError("Added message of different hexident.")
+
+		if message.message_type == 'MSG':
+			self.messages.append(message)
+
+	@property
+	def last_position(self):
+		"""Finds the last known position of the flight (by iterating backwards through collected messages).
+
+		Returns:
+			tuple: a tuple of :py:class:`float` if the position was ever recorded, (None, None) otherwise.
+
+		"""
+		for message in reversed(self.messages):
+			if message.latitude and message.longitude:
+				return (message.latitude, message.longitude)
+		return None, None
+
+	@property
+	def last_altitude(self):
+		"""Finds the last known altitude of the flight (by iterating backwards through collected messages).
+
+		Returns:
+			float: the altitude in feet, None otherwise.
+
+			"""
+		for message in reversed(self.messages):
+			if message.altitude:
+				return message.altitude
+		return None
+
+	@property
+	def path(self):
+		"""Reconstructs the flight path. Yields it as an iterator.
+
+		Yields:
+			tuple: (lat, lon, alt) describing the latitude, longitude and altitude of a message
+
+		"""
+		for message in self.messages:
+			lat, lon, alt = message.latitude, message.longitude, message.altitude
+			if lat and lon and alt:
+				yield (lat, lon, alt)
+
+
+	def __iter__(self):
+		return iter(self.messages)
+
